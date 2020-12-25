64f937ed7ade8a9b8beb345123ae36c2eacbc993
Nelson-Gon <gonzabato@hotmail.com>
2020-12-26 00:16:27 +0800

    Fix issues with abs paths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 
